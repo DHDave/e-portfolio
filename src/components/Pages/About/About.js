ef36420679c7d15e7c4e8164f8df96bbf6e07ee0
David Hajos <dh.it.email@gmail.com>
2019-02-22 15:06:00 +0000

    7 # Introduce LinkedIn and GitHub links

--- src/components/Pages/About/About.js
+++ src/components/Pages/About/About.js
@@ -8,22 +8,41 @@ import reactJS from './Images/reactJS.png';
 import mongo from './Images/mongoDB.png';
 import nodeJS from './Images/nodeJS.png';
 import Stack from './Stack';
+import gitHub from './Images/github.svg';
+import linkedIn from './Images/linkedIn1.svg';
 
 const About = () => {
     return (
         <div>
             <div className='textbox-title' >
                 <p>About</p>
+                <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.7.2/css/all.css" integrity="sha384-fnmOCqbTlWIlj8LyTjo7mOUStjsKC4pOpQbqyi7RrhN7udi9RwhKkMHpvLbHG9Sr" crossorigin="anonymous"></link>
             <div/>
             <br />
             <div className='container' >
-                <div className='aside-1'></div>
+                <div className='aside-1'>
+                <div className='logos'>        
+                        <a href='https://github.com/DHDave'>
+                            <  img src={gitHub} alt = 'GitLogo' ></img>
+                        </a>
+                        <a href='https://www.linkedin.com/in/david-hajos'>
+                            <img src={linkedIn} alt = 'LinkedInLogo' ></img>
+                        </a>
+                    </div>
+                </div>
                 <div className='textbox-main'>I am a junior Full Stack Web Developer and UI/UX Javascript specialist. Check out my latest projects on the web portfolio page and feel free to take a look at my GitHub repositories to follow up the codes.</div>
                 <div className='aside-2'></div>
             </div>
-            <br /><br />
-           <div className='textbox-sub'>Click on the element of your interest below to x-ray its link to this website.</div>
-
+            <br />
+            {/* <div className='logos'>
+                <a href='https://github.com/DHDave'>
+                    <  img src={gitHub} alt = 'GitLogo' ></img>
+                </a>
+                <a href='https://www.linkedin.com/in/david-hajos'>
+                    <img src={linkedIn} alt = 'LinkedInLogo' ></img>
+                </a>
+            </div> */}
+            <br />
             <div className='container' >
                 < Stack name='JavaScript' logo={javascript} />
                 < Stack name='ReactJS' logo={reactJS} />
@@ -41,3 +60,5 @@ const About = () => {
 
 
 export default About;
+
+
